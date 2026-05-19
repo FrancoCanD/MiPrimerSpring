72a5dd37e4074fd19778bb181e832512ea847ae4
soledad-fernandez@live.cl <soledad-fernandez@live.cl>
2026-05-18 20:44:15 -0400

    Se crea rama develop_sf01, se crea index del programa y se crea clase asistencia

--- src/main/java/cl/kibernum/miprimerspringboot/bl/entity/Asistencia.java
+++ src/main/java/cl/kibernum/miprimerspringboot/bl/entity/Asistencia.java
@@ -1,4 +1,52 @@
 package cl.kibernum.miprimerspringboot.bl.entity;
 
-public class Asistencia {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Entidad Asistencia, muestra la asistencia registrada para los estudiantes
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "asistencias")
+
+public class Asistencia{
+    @Id
+    private Long id;
+    /**
+     *
+     */
+    @Column(nullable = false)
+    private LocalDateTime registro;
+    /**
+     *
+     */
+    @Column(nullable = false)
+    private LocalDate fechaClase;
+    /**
+     *
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "persona_id", referencedColumnName = "id")
+    private Persona persona;
+    /**
+     *
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clase_id", referencedColumnName = "clase")
+    private Clase clase;
+
+
+
+
 }
