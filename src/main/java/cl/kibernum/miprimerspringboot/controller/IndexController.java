fb0cfc5d7fd6896c317b0ac2f2d4b37d63bc8271
soledad-fernandez@live.cl <soledad-fernandez@live.cl>
2026-05-27 20:32:35 -0400

    se actulizan estilos del proyecto esculakenpo

--- src/main/java/cl/kibernum/miprimerspringboot/controller/IndexController.java
+++ src/main/java/cl/kibernum/miprimerspringboot/controller/IndexController.java
@@ -31,6 +31,6 @@ public class IndexController {
         model.addAttribute("totalClases", claseService.listarClases().size());
         model.addAttribute("totalAsistencias", asistenciaService.listarTodas().size());
 
-        return "index"; // Busca el archivo index.html en templates/
+        return "index"; // Busca el archivo footer.html en templates/
     }
 }
