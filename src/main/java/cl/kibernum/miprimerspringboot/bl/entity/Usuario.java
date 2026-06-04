02355e92e4a3b67688880da49edd621c420eaf3d
FrancoCanD <f.cancinodiaz@gmail.com>
2026-06-03 23:05:42 -0400

    Creación entidad Usuarios y sus atributos

---
+++ src/main/java/cl/kibernum/miprimerspringboot/bl/entity/Usuario.java
@@ -0,0 +1,36 @@
+package cl.kibernum.miprimerspringboot.bl.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Entidad Estudiante, representa a los alumnos inscritos
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    @NotBlank(message = "Debe ingresar un nombre de usuario")
+    private String username;
+
+    @Column(nullable = false, length = 20)
+    @NotBlank(message = "Debe ingresar una contraseña")
+    private String password;
+
+    @Column(nullable = false)
+    @NotNull(message = "Debe seleccionar el estado del usuario")
+    private Boolean activo = true;
+}
