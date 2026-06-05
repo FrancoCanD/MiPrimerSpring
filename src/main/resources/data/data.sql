d8e69f8b4ea900a3a5bc33afb3cd882e9d599752
Felipe Kessi <felipe@kessi.cl>
2026-06-04 20:46:42 -0400

    actualización data.sql, se cambia el nombre a constraint tabla usuario_rol

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -83,10 +83,10 @@ CREATE TABLE usuario_rol(
                             usuario_id INT NOT NULL,
                             rol_id INT NOT NULL,
                             PRIMARY KEY (usuario_id, rol_id),
-                            CONSTRAINT fk_usuario_rol_usuario
+                            CONSTRAINT fk_usuario_rol_usuarios
                                 FOREIGN KEY (usuario_id)
                                 REFERENCES usuarios(id),
-                            CONSTRAINT fk_usuario_rol_rol
+                            CONSTRAINT fk_usuario_rol_roles
                                 FOREIGN KEY (rol_id)
                                 REFERENCES roles(id)
 );
